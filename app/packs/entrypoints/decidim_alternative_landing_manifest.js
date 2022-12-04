a1105cd1419bcea625789655914875575778b169
Alexander Rusa <alex@rusa.at>
2022-12-04 23:02:50 +0000

    Change assets for webpacker

---
+++ app/packs/entrypoints/decidim_alternative_landing_manifest.js
@@ -0,0 +1,5 @@
+import "src/decidim/alternative_landing/alternative_landing"
+import "src/decidim/alternative_landing/calendar"
+
+// CSS
+import "stylesheets/decidim/alternative_landing/alternative_landing.scss";
