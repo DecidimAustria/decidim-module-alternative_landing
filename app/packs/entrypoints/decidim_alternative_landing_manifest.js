462b7a13b6d0a0fe7232492593ab797b6ccda7bd
Alexander Rusa <alex@rusa.at>
2022-12-05 08:18:42 +0000

    Add extensions to js imports

--- app/packs/entrypoints/decidim_alternative_landing_manifest.js
+++ app/packs/entrypoints/decidim_alternative_landing_manifest.js
@@ -1,5 +1,5 @@
-import "src/decidim/alternative_landing/alternative_landing"
-import "src/decidim/alternative_landing/calendar"
+import "src/decidim/alternative_landing/alternative_landing.js.es6"
+import "src/decidim/alternative_landing/calendar.js.es6"
 
 // CSS
 import "stylesheets/decidim/alternative_landing/alternative_landing.scss";
