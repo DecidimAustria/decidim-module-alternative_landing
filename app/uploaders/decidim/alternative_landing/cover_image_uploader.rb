b1bd47f735c4c966d8b48098772200cff5dd1f10
Alexander Rusa <alex@rusa.at>
2022-12-05 08:56:39 +0000

    Change uploader version to set_variants

--- app/uploaders/decidim/alternative_landing/cover_image_uploader.rb
+++ app/uploaders/decidim/alternative_landing/cover_image_uploader.rb
@@ -4,8 +4,8 @@ module Decidim
   module AlternativeLanding
     # This class deals with uploading cover images to content blocks.
     class CoverImageUploader < RecordImageUploader
-      version :big do
-        process resize_to_fit: [2880, 1620]
+      set_variants do
+        { big: { resize_to_fit: [2880, 1620] } }
       end
 
       def max_image_height_or_width
